f2768d2ce93750cfef1575f43b42ec7a8c93bc13
Zwampyh <ich3n3y@gmail.com>
2022-09-13 23:08:42 -0600

    Cleaned up a bit

--- src/main/java/com/revature/ORM/Testing/Driver.java
+++ src/main/java/com/revature/ORM/Testing/Driver.java
@@ -9,14 +9,17 @@ import com.revature.ORM.Database.Database;
 public class Driver {
 	
 	public static void main(String[] args) throws SQLException, NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+		
+		/*
+		 * This class is just a main method with examples to show how to use the ORM and to test it's working with my personal database.
+		 * This class is not necessary but the classes ObjTest1 and ObjTest2 are used in the Unit tests
+		 */
+		
+		
+		
+		
 		//Database db = new Database("jdbc:postgresql://postgres.ccyxl0ztd58i.us-west-2.rds.amazonaws.com:5432/", System.getenv("db_name"), System.getenv("db_pass"));
-
-		
-		AccountDao aDao = new AccountDao();
-		Account acc = aDao.getAccountById(1);
-		//System.out.println(acc.acc_balance);
-		
-		
+	
 		//ObjTest1 Davinci = new ObjTest1();
 		//Davinci.usernames = "Davinci";
 		//Davinci.passwords = "Dinci";
@@ -28,14 +31,11 @@ public class Driver {
 		//Object query =  db.where("Davinci", "login", "usernames", ObjTest1.class);
 		//ObjTest1 row = (ObjTest1) query;
 		
-		//System.out.println();
-		//System.out.println(row.usernames);
-		//System.out.println(row.passwords);
 		
 		//ObjTest2 acco = new ObjTest2();
 		//acco.usernames = "Davinci";
-	//	acco.acc_balance = 2.0;
-	//	acco.acc_num = 4.0;
+		//acco.acc_balance = 2.0;
+		//acco.acc_num = 4.0;
 		
 		//db.insert(acco);
 		
